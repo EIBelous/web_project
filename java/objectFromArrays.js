b76615c7b0a10c26e2af1499c5eb5a879b3f61e2
EIBelous <evgen.i.bilous@gmail.com>
2021-12-19 20:15:06 +0200

    done

---
+++ java/objectFromArrays.js
@@ -0,0 +1,9 @@
+
+let getZippedArrays=function(keysArray,data){
+    let obj={}
+    for(let i=0; i<keysArray.length; i++){
+
+        obj[keysArray[i]]=data[i]}
+    return obj}
+getZippedArrays()
+console.log(obj)
