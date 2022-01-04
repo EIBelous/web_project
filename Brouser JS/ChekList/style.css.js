34ccff8557f278d2d018581f1546bfc364a04a4a
EIBelous <evgen.i.bilous@gmail.com>
2022-01-04 21:34:12 +0200

    finished

--- Brouser JS/ChekList/style.css.js
+++ Brouser JS/ChekList/style.css.js
@@ -3,12 +3,14 @@ var items = list.children;
 var emptyListMessage = document.querySelector('.empty-tasks');
 var newItemForm = document.querySelector('.add-form');
 var newItemTitle = newItemForm.querySelector('.add-form-input');
-var taskTemplate = document.querySelector('#task-template');
-console.log(taskTemplate);
+var taskTemplate = document.querySelector('#task-template').content;
+var newItemTemplate = taskTemplate.querySelector('.todo-list-item');
 
 var toggleEmptyListMessage = function () {
     if (items.length === 0) {
         emptyListMessage.classList.remove('hidden');
+    } else {
+        emptyListMessage.classList.add('hidden');
     }
 };
 
@@ -28,4 +30,12 @@ newItemForm.addEventListener('submit', function (evt) {
     evt.preventDefault();
 
     var taskText = newItemTitle.value;
+    var task = newItemTemplate.cloneNode(true);
+    var taskDescription = task.querySelector('span');
+    taskDescription.textContent = taskText;
+    addCheckHandler(task);
+
+    list.appendChild(task);
+    toggleEmptyListMessage();
+    newItemTitle.value = '';
 });
